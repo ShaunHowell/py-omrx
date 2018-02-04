9cbb362a9707d0c094113d73ae7dee8d036bc02e
Shaun Howell <shaunkhowell@gmail.com>
2018-02-04 15:51:42 +0000

    improved folder structure. added demo. added tests.

---
+++ src/tests/test_split_pdf.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from omr.preprocessing import *
+
+
+# class TestSplit_pdf(TestCase):
+#     def test_split_pdf(self):
+#         # run split_pdf then check that the correct number of pdf files have been produced
+#         with open('tests/in/split_pdf.pdf') as in_file:
+#             pass
+#         self.fail()
