94762e89ff2a27bd5dd946ac3c5fe39f6335857f
shaun howell <shaunkhowell@gmail.com>
2019-08-21 11:52:36 +0100

    version bump and test

--- pyomrx/tests/test_formatting.py
+++ pyomrx/tests/test_formatting.py
@@ -1,5 +1,9 @@
 import subprocess
-
+import pyomrx
 
 def test_src_yapf_formatted():
     assert subprocess.call(['yapf', '--diff', '-r', './pyomrx']) == 0
+
+def test_version_bumped():
+    tags = subprocess.run(['git','tag'],stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
+    assert not any([pyomrx.__version__ in tag for tag in tags])
