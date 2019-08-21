14e0ea5312ff5edde17537e7470f2e71d1b70857
shaun howell <shaunkhowell@gmail.com>
2019-08-21 12:22:28 +0100

    dont check verion bump if on master already

--- pyomrx/tests/test_formatting.py
+++ pyomrx/tests/test_formatting.py
@@ -10,4 +10,8 @@ def test_version_bumped():
     tags = subprocess.run(
         ['git', 'tag'],
         stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-    assert not any([pyomrx.__version__ in tag for tag in tags])
+    branch = subprocess.run(
+        'git rev-parse --abbrev-ref HEAD',
+        stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n')
+    if branch != 'master':
+        assert not any([pyomrx.__version__ in tag for tag in tags])
