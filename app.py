9b60c781902ed096e7b395e13fe3a4ee21a622e3
shaun howell <shaunkhowell@gmail.com>
2020-02-05 23:21:44 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 import sys
 sys.path.append(str(Path.cwd() / 'pyomrx'))
-from pyomrx.gui import pyomrx_gui
-
-
-def main():
-    pyomrx_gui.main()
+from pyomrx.gui.app import main
 
 
 if __name__ == '__main__':
--- pyomrx/__init__.py
+++ pyomrx/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 IMAGE_SUFFIXES = ['.png', '.jpg', 'jpeg', '.PNG', '.JPG', '.JPEG']
